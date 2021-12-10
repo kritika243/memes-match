7ff01b620b83191d6bb0dba523f8a56138c2bacd
kritika243 <24kritikasharma24@gmail.com>
2021-12-10 17:13:34 +0530

    heading

--- src/App.js
+++ src/App.js
@@ -6,9 +6,17 @@ import { shuffle } from 'lodash'
 function App() {
   const [cards, setCards] = useState(shuffle([...Images, ...Images]))
   const [activeCards, setActiveCards] = useState([])
+  const [won, setWon] = useState(false)
   const [foundMatches, setFoundMatches] = useState([])
+  const [clicks, setClicks] = useState(0)
 
   function flipCard(index) {
+    if (won) {
+      setCards(shuffle([...Images, ...Images]))
+      setFoundMatches([])
+      setWon(false)
+      setClicks(0)
+    }
     if (activeCards.length === 0) {
       setActiveCards([index])
     }
@@ -16,6 +24,9 @@ function App() {
       const firstIndex = activeCards[0]
       const secondIndex = index
       if (cards[firstIndex] === cards[secondIndex]) {
+        if (foundMatches.length + 2 === cards.length) {
+          setWon(true)
+        }
         setFoundMatches([...foundMatches, firstIndex, secondIndex])
       }
       setActiveCards([...activeCards, index])
@@ -23,9 +34,11 @@ function App() {
     if (activeCards.length === 2) {
       setActiveCards([])
     }
+    setClicks(clicks + 1)
   }
   return (
     <div>
+      <h1>Memes Match</h1>
       <div className='container'>
         {cards.map((card, index) => {
           const flippedToFront =
@@ -45,6 +58,12 @@ function App() {
             </div>
           )
         })}
+      </div>
+      <div className='score'>
+        {won && <h3>You Won the game, Congratulations!</h3>}
+        <br />
+        Clicks : {clicks} &nbsp; &nbsp; You found : {foundMatches.length / 2}{' '}
+        pairs
       </div>
     </div>
   )
