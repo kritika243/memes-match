3d0bfd128310a276c5535dfd54825b6ee4fb5142
kritika243 <24kritikasharma24@gmail.com>
2021-12-10 11:25:31 +0530

    + lodash to shuffle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import Images from './Images'
+import { shuffle } from 'lodash'
 
 function App() {
-  const [cards, setCards] = useState([...Images, ...Images])
+  const [cards, setCards] = useState(shuffle([...Images, ...Images]))
   return (
     <div>
       <div className='container'>
